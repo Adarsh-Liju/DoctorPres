c22462337cf90d72a055f5ad264841869f1b76f9
Adarsh Liju Abraham <adarsh.liju.abraham@gmail.com>
2022-10-18 11:44:21 +0530

    Hello

--- login.py
+++ login.py
@@ -8,10 +8,12 @@ with st.form("my_form",clear_on_submit=True):
     st.write("You entered:",patient_age)
     symptoms=st.text_input("Please enter your symptoms")
     st.write("You entered",symptoms)
+    camera=st.camera_input("Take a picture",key="camera")
+    if camera is not None:
+        st.image(camera)
     submitted = st.form_submit_button("Submit")
     if submitted:   
         st.write("## Thank you for your response :-) ")
 button=st.button("Click me for Medicine list")    
 if button:
     os.system("streamlit run main.py")
-    
---
+++ search.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.sidebar.title("DoctorPres")
+st.sidebar.subheader("A Doctor's Prescription App")
+
+query=st.text_input("Search Here")
